4ce3e24b4a32fa6f71858a1ca92e9d68537ecc4e
rahma-mohmed <engrahma1422003@gmail.com>
2024-08-20 20:18:53 +0200

    Add FileStream folder

---
+++ FileStream/IOStream/CADisposableDesingPattern/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace CADisposableDesingPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*CurrencyService currencyService = null;
+            try
+            {
+                currencyService = new CurrencyService();
+                var res = currencyService.GetCoins();
+                Console.WriteLine(res);
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex);
+            }
+            finally {
+                currencyService?.Dispose();
+            }
+            Console.ReadKey();*/
+
+            //using call dispose by default
+            using (CurrencyService currencyService = new CurrencyService())
+            {
+                var res = currencyService.GetCoins();
+                Console.WriteLine(res);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class CurrencyService : IDisposable
+    {
+        //unmanaged => clean
+        private readonly HttpClient _httpClient;
+        private bool _disposed = false;
+
+        public CurrencyService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        ~CurrencyService()
+        {
+            Dispose(false);
+        }
+
+        //public
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        //private => class , any person inherit
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+
+            if (disposing)
+            {
+                _httpClient.Dispose();
+            }
+            _disposed = true;
+        }
+
+        public string GetCoins()
+        {
+            string url = "https://coinbase.com/api/v2/currencies";
+            var res = _httpClient.GetStringAsync(url).Result;
+            return res;
+        }
+    }
+}
---
+++ FileStream/IOStream/CAFileStream/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace CAFileStream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Method02();
+            Console.ReadKey();
+        }
+
+        static void Method01()
+        {
+            string path = "D:\\DEPI\\FileStream\\IOStream\\users.csv";
+
+            using (FileStream fs = new FileStream(path, FileMode.Open , FileAccess.ReadWrite))
+            {
+                byte[] data = new byte[fs.Length];
+
+                int numBytesToRead = (int) fs.Length;
+                int numBytesRead = 0;
+
+                while (numBytesToRead > 0) { 
+                    int n = fs.Read(data, numBytesRead, numBytesToRead);
+
+                    if(n == 0)
+                    {
+                        break; 
+                    }
+                    numBytesToRead -= n;
+                    numBytesRead += n;
+                }
+
+                foreach (byte b in data)
+                {
+                    Console.WriteLine(b);
+                }
+            }
+        }
+
+        static void Method02()
+        {
+            var path2 = "D:\\DEPI\\FileStream\\IOStream\\t1.txt";
+
+            using (var fs2 = new FileStream(path2, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                fs2.Seek(10, SeekOrigin.Begin);
+                fs2.WriteByte(65);
+                fs2.Position = 0;
+
+                for (int i = 0; i < fs2.Length; i++)
+                {
+                    Console.WriteLine(fs2.ReadByte());
+                }
+            }
+        }
+    }
+}
